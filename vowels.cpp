01666184b7b7168cc4a1621ce405a08e323d1ac3
avi132005 <avikumarpandey1@gmail.com>
2026-02-13 01:01:30 +0530

    working with string and learning how string works with the help of simple program: FINDING NUMBER OF VOWELS IN A STRING

--- vowels.cpp
+++ vowels.cpp
@@ -3,7 +3,17 @@ using namespace std;
 int main()
 {
     string word;
-    cin>>word;
-    cout<<word;
+    cout<<"input any string:";
+    getline(cin , word);
+    int count = 0;
+    for(auto j = 0; j < word.length() ; j++)
+    {
+        char i = word[j];
+        if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u'||i=='A'||i=='E'||i=='I'||i=='O'||i=='U')
+        {
+            count += 1;
+        }
+    }
+    cout<<"number of vowels in "<<word<<" is:"<<count;
     return 0;
 }
