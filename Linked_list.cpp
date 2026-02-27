12204c5c5bd108c6afd88acf5764ff70b874267e
avi132005 <avikumarpandey1@gmail.com>
2026-02-28 00:33:23 +0530

    learnig basic of linked list

---
+++ Linked_list.cpp
@@ -0,0 +1,53 @@
+//to insert eleement at first place 
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+Node(int value)
+{
+    data = value;
+    next = NULL;
+}
+};
+void print_list(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    Node* head = NULL;
+    Node* second = new Node(20);
+    head = second;
+    Node* third = new Node(30);
+    second->next = third;
+    Node* temp = head;
+    cout<<"elements in list are: ";
+    print_list(head);
+    Node* first = new Node(10);
+    first->next = head;
+    head = first;
+     temp = head;
+     cout<<"after inserting node at starting : ";
+     print_list(head);
+    Node* fourth = new Node(40);
+    temp = head;
+    while(temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next= fourth;
+    temp = head;
+    cout<<"after inserting at end: ";
+    print_list(head);
+    return 0;
+
+}
